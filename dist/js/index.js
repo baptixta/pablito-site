8817b19f9be087162a38d6cf91fcc033f9c581bf
baptixta <daviarcosbaptista@gmail.com>
2020-08-03 22:52:01 -0300

    pablito // update empreendimentos

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,8 @@
 /*! project-name v0.0.1 | (c) 2020 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
 let title = document.querySelector('.home__title');
 let description = document.querySelector('.home__description');
-let arrow = document.querySelector('.home__contact div');
-let contact = document.querySelector('.home__contact');
+let arrow = document.querySelectorAll('.home__contact div');
+let contact = document.querySelectorAll('.home__contact__link');
 let icons = document.querySelectorAll('.social-media__icon');
 let buildings = document.querySelector('.buildings path');
 
@@ -10,7 +10,7 @@ let tl = gsap.timeline({});
 
 tl.fromTo(title, {opacity: 0}, {opacity: 1, duration: 1.5, delay: .5}, 0);
 tl.fromTo(description, {opacity: 0, y: -10}, {opacity: 1, y: 0, duration: .7, delay: 1.5}, 0)
-tl.fromTo(contact, {opacity: 0, y: 15}, {opacity: 1, y: 0, duration: .3, delay: 2}, 0)
-tl.fromTo(arrow, {opacity: 0, x: -20}, {opacity: 1, x: 0, duration: .7, delay: 2.6}, 0)
+tl.staggerFromTo(contact, .1, {opacity: 0, y: 15}, {opacity: 1, y: 0, duration: .3, delay: 2}, .2, 0)
+tl.staggerFromTo(arrow, .3, {opacity: 0, x: -20}, {opacity: 1, x: 0, duration: .7}, .2)
 tl.staggerFromTo(icons, .2, {x: -5},{x: 0,opacity: 1, delay: 2.8,}, .1, 0)
 tl.fromTo(buildings, {opacity: 0}, {opacity: .4, delay: 3.5}, 0)
